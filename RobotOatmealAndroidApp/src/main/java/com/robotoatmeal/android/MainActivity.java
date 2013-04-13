54c6d4c23d959519829260b800e68c1d594b84c4
app_5caf5ea3 <app_5caf5ea3@ip-10-82-222-220.ec2.internal>
2013-04-13 20:47:31 +0000

    AndroidKickstartR generation

---
+++ RobotOatmealAndroidApp/src/main/java/com/robotoatmeal/android/MainActivity.java
@@ -0,0 +1,46 @@
+
+package com.robotoatmeal.android;
+
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.Background;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.UiThread;
+import com.googlecode.androidannotations.annotations.ViewById;
+import com.googlecode.androidannotations.annotations.rest.RestService;
+import com.robotoatmeal.android.rest.RestClient;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity
+    extends Activity
+{
+
+    @ViewById
+    TextView hello;
+    @RestService
+    RestClient restClient;
+
+    @AfterViews
+    void afterViews() {
+    }
+
+    @UiThread
+    void doSomethingElseOnUiThread() {
+        hello.setText("Hi!");
+    }
+
+    @Background
+    void doSomethingInBackground() {
+        restClient.main();
+        doSomethingElseOnUiThread();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater();
+        return true;
+    }
+
+}
