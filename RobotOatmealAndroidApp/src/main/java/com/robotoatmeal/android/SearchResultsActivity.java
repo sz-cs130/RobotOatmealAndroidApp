ee9f708431e3fab38d732c2be937564280e1c6e8
Bryan Ngo <bngo92@gmail.com>
2013-04-26 20:45:53 -0700

    Fixed action bar

--- RobotOatmealAndroidApp/src/main/java/com/robotoatmeal/android/SearchResultsActivity.java
+++ RobotOatmealAndroidApp/src/main/java/com/robotoatmeal/android/SearchResultsActivity.java
@@ -24,7 +24,7 @@ public class SearchResultsActivity extends Activity {
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	private void setupActionBar() {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			//getActionBar().setDisplayHomeAsUpEnabled(true);
+			getActionBar().setDisplayHomeAsUpEnabled(true);
 		}
 	}
 
