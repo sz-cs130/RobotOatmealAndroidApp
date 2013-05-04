2ce2ee08ee9cd985fdd64a0788f4ece0e520113e
Bryan Ngo <bngo92@gmail.com>
2013-05-03 20:49:47 -0700

    Added coupon detail activity

---
+++ RobotOatmealAndroidApp/src/main/java/com/robotoatmeal/android/CouponDetailActivity.java
@@ -0,0 +1,87 @@
+package com.robotoatmeal.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_coupon_detail)
+public class CouponDetailActivity extends Activity {
+	
+	Coupon coupon;
+	
+	@ViewById
+	TextView description;
+	
+	@ViewById
+	TextView merchantId;
+		
+	@ViewById
+	TextView couponCode;
+	
+	@AfterViews
+	void updateText() {
+		// TODO: debug exception
+		try {
+		description.setText(coupon.description);
+		merchantId.setText("" + coupon.merchantId);
+		couponCode.setText(coupon.couponCode);
+		} catch (Exception e) {
+			Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+			couponCode.setText(e.toString());
+		}
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_coupon_detail);
+		// Show the Up button in the action bar.
+		setupActionBar();
+		String detail = getIntent().getStringExtra("detail");
+		Gson gson = new Gson();
+		coupon = gson.fromJson(detail, Coupon.class);
+	}
+
+	/**
+	 * Set up the {@link android.app.ActionBar}.
+	 */
+	private void setupActionBar() {
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.coupon_detail, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
