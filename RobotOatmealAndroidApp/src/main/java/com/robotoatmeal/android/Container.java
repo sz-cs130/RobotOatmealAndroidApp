abb7c15733be9425b995c77bda5da01e94945b17
Bryan Ngo <bngo92@gmail.com>
2013-05-03 20:46:34 -0700

    Added json format class files

---
+++ RobotOatmealAndroidApp/src/main/java/com/robotoatmeal/android/Container.java
@@ -0,0 +1,12 @@
+package com.robotoatmeal.android;
+
+import java.util.List;
+
+public class Container {
+	
+	List<Coupon> coupon;
+	String metaInfo;
+	int includedResults;
+	int totalResults;
+	
+}
---
+++ RobotOatmealAndroidApp/src/main/java/com/robotoatmeal/android/Coupon.java
@@ -0,0 +1,12 @@
+package com.robotoatmeal.android;
+
+class Coupon {
+	
+	String description;
+	int merchantId;
+	String couponCode;
+	int couponUsagesCount;
+	String expirationDate;
+	String lastUsedDate;
+	
+}
