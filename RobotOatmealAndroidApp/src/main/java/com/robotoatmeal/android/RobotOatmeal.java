576b99426a8815de43833386e1fc0c876ab54beb
eric sun <eric.sun@ucla.edu>
2013-05-18 20:04:48 -0700

    added global variable class; extends Application

---
+++ RobotOatmealAndroidApp/src/main/java/com/robotoatmeal/android/RobotOatmeal.java
@@ -0,0 +1,9 @@
+package com.robotoatmeal.android;
+
+import android.app.Application;
+
+public class RobotOatmeal extends Application
+{
+	public CouponContainer savedSearchResults = null;
+	public String savedSearchQuery = null;
+}
